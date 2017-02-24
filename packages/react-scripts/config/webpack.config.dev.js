de73bae97588d5cf55bf1bd8fff99a153ef617ee
5argon <pyasry@gmail.com>
2017-02-24 21:06:34 +0700

    Have both PostCSS and Relay

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -184,14 +184,7 @@ module.exports = {
               ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
               plugins: function () {
                 return [
-                  autoprefixer({
-                    browsers: [
-                      '>1%',
-                      'last 4 versions',
-                      'Firefox ESR',
-                      'not ie < 9', // React doesn't support IE8 anyway
-                    ]
-                  })
+                require('postcss-cssnext')
                 ]
               }
             }
--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -195,16 +195,9 @@ module.exports = {
               options: {
                 ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
                 plugins: function () {
-                  return [
-                    autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ]
-                    })
-                  ]
+                    return [
+                        require('postcss-cssnext')
+                    ]
                 }
               }
             }
